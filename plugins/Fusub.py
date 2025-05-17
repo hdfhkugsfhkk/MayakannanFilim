3ff6c28cf5abef8f0c9634af825ba8978a27ee11
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-05-18 01:33:15 +0530

    Update Fusub.py

--- plugins/Fusub.py
+++ plugins/Fusub.py
@@ -1,2 +1,11 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 FUB_TEXT =
+
+@Client.on_message(filters.command("help"))
+async def generate_link(client, message):
+    command_text = message.text.split(maxsplit=1)
+    if len(command_text) < 2:
+        await message.reply("FUB_TEXT")
+        return
