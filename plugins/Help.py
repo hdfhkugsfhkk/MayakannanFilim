06cea8f9cb6c3c0b2045e0b726abd29f9d482512
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-06-06 00:39:11 +0530

    Update Help.py

--- plugins/Help.py
+++ plugins/Help.py
@@ -5,65 +5,37 @@ from info import ADMINS
 FUB_TEXT ="""<b><blockquote>You can find the bot commands here.</blockquote>
 
 ‣ /logs - Get logs as a file
-
 ‣ /restart - Restart the bot
-
 ‣ /stats - Get bot user stats (Will send only after checking active users & Get count of total files in DB
-
 ‣ /broadcast - Reply to a message to send that to all bot users
-
 ‣ /deleteall - This will delete all indexed files.
-
 ‣ /settings - Group settings 
-
 ‣ /gfilter - Global filter Adding
-
 ‣ /viewgfilters - Total Global filter Show
-
 ‣ /delg - special Global filter Delete
-
 ‣ /delallg - All Global filter Delete
-
 ‣ /filter - adding Filter
-
 ‣ /filters - show all Filters
-
 ‣ /del - Global filter Delete
-
 ‣ /delall - All Filter Delete
-
 ‣ /deletefiles - special movie files delete
-
 ‣ /deletesmallfiles - 
-
 ‣ /delete_duplicate - 
-
 ‣ /link - Movie Link Carte
-
 ‣ /index - Start indexing a database channel (bot must be admin of the channel if that is private channel)
 You can just forward the message from database channel for starting indexing, no need to use the /index command
 /indexlink - Start indexing a database channel using link (bot must be admin of the channel if that is private channel)
 /indexlink  or /indexlink  
-
 ‣ /delete - Reply to a file to delete it from database
-
 ‣ /setchat1 - <i>Set force subscribe channel - /setchat1 channel_id (Answer the questions after that) Bot must be admin of that channel (Bot will create a new invite link)</i>
-
 ‣ /setchat2 - <i>Set force subscribe channel - /setchat2 channel_id (Answer the questions after that) Bot must be admin of that channel (Bot will create a new invite link)</i>
-
 ‣ /delchat1 - <i>delete Force subscribe channel</i>
-
 ‣ /delchat2 - <i>delete Force subscribe channel</i>
-
 ‣ /viewchat1 - <i>Check invite link for force subscribe channel</i>
-
 ‣ /viewchat2 - <i>Check invite link for force subscribe channel</i>
-
 ‣ /purge_one - <i>Clear all force subscribe users from db</i>
-
 ‣ /purge_two - <i>Clear all force subscribe users from db</i>
-
-‣ /totalreq - <i>Total all force subscribe Count</i></b>"""
+‣ /totaltalreq - <i>Total all force subscribe Count</i></b>"""
 
 @Client.on_message(filters.command("help") & filters.private & filters.user(ADMINS))
 async def generate_link(client, message):
