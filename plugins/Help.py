87bbd0b905827cc7cbc337ea8f4504662672ef49
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-06-05 23:59:05 +0530

    Update Help.py

--- plugins/Help.py
+++ plugins/Help.py
@@ -1,20 +1,29 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import ADMINS
 
 FUB_TEXT ="""<b><blockquote>Force Subscription Management
 <i>Set, manage, or clear force subscribe channels.</i></blockquote>
 
 ‣ /setchat1 - <i>Set force subscribe channel - /setchat1 channel_id (Answer the questions after that) Bot must be admin of that channel (Bot will create a new invite link)</i>
+
 ‣ /setchat2 - <i>Set force subscribe channel - /setchat2 channel_id (Answer the questions after that) Bot must be admin of that channel (Bot will create a new invite link)</i>
+
 ‣ /delchat1 - <i>delete Force subscribe channel</i>
+
 ‣ /delchat2 - <i>delete Force subscribe channel</i>
+
 ‣ /viewchat1 - <i>Check invite link for force subscribe channel</i>
+
 ‣ /viewchat2 - <i>Check invite link for force subscribe channel</i>
+
 ‣ /purge_one - <i>Clear all force subscribe users from db</i>
+
 ‣ /purge_two - <i>Clear all force subscribe users from db</i>
+
 ‣ /totalreq - <i>Total all force subscribe Count</i></b>"""
 
-@Client.on_message(filters.command("fusub"))
+@Client.on_message(filters.command("fusub") & filters.private & filters.user(ADMINS))
 async def generate_link(client, message):
     command_text = message.text.split(maxsplit=1)
     if len(command_text) < 2:
